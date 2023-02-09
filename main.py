9b19776194c7d9dd7703e69179d1a2d567753ab4
mbookamer <79340162+mbookamer@users.noreply.github.com>
2023-02-09 00:10:12 -0600

    Update main.py
    added test cases, created the appropriate function calls, and made functions to call comparisons.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ CMPS 2200  Recitation 2
 ### the only imports needed are here
 import tabulate
 import time
+import math
 ###
 
 #I coded this out in VS code and only put the final steps here for simplicity and clarity
@@ -35,7 +36,7 @@ def test_simple_work():
 	assert work_calc(20, 4, 2) == 1644
 	assert work_calc(10, 2, 5) == 18
 	
-pytest main.py::test_simple_work
+#pytest main.py::test_simple_work
 
 def work_calc(n, a, b, f):
 	"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
@@ -53,7 +54,7 @@ def work_calc(n, a, b, f):
 	if n <= 1:
 		return 1 #base case
 	else: 
-		a*work_calc(n//b, a, b, f) + f(n) #recursive step that only works with integer values as opposed to simple_work
+		return a*work_calc(n//b, a, b, f) + f(n) #recursive step that only works with integer values as opposed to simple_work
 	pass
 
 def span_calc(n, a, b, f):
@@ -72,7 +73,7 @@ def span_calc(n, a, b, f):
 	if n<= 1:
 		return 1 #base case
 	else:
-		return a*work_calc(n//b, a, b, f) + f(n) #however, we know for span a will always =1
+		return a*work_calc(n//b, a, b, f) + f(n) 
 	pass
 
 def test_work():
@@ -80,9 +81,9 @@ def test_work():
 	assert work_calc(10, 2, 2,lambda n: 1) == 15 #TODO
 	assert work_calc(20, 1, 2, lambda n: n*n) == 530 #TODO
 	assert work_calc(30, 3, 2, lambda n: n) == 300 #TODO
-	assert work_calc(16, 1, 2,lambda n: n) ==
-	assert work_calc(10, 2, 2,lambda n: 1) ==
-	assert work_calc(10, 2, 2,lambda n: 1) ==
+	assert work_calc(16, 1, 2,lambda n: n) == 31
+	assert work_calc(8, 1, 2,lambda n: 1) == 4
+	assert work_calc(24, 3, 2,lambda n: n*n) == 1656
 
 def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
 	"""
@@ -94,19 +95,21 @@ def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000])
 	(n, work_fn1(n), work_fn2(n), ...)
 	
 	"""
-	result = []
-	for n in input_sizes:
+	result_w = []
+	for n in sizes:
 		# compute W(n) using current a, b, f
-		result.append((
+		result_w.append((
 			n,
 			work_fn1(n),
 			work_fn2(n)
 			))
-	return result
+	return result_w
 
-def print_results(results):
+
+
+def print_result_work(result_w):
 	""" done """
-	print(tabulate.tabulate(results,
+	print(tabulate.tabulate(result_w,
 							headers=['n', 'W_1', 'W_2'],
 							floatfmt=".3f",
 							tablefmt="github"))
@@ -118,8 +121,58 @@ def test_compare_work():
 	# create work_fn1
 	# create work_fn2
 
+    def work_fn1(n):
+        return work_calc(n, 2, 2, lambda n: n)
+    
+    def work_fn2(n):
+        return work_calc(n, 2, 2,lambda n: math.log(n, 2))
+
     res = compare_work(work_fn1, work_fn2)
-	print(res)
+    print_result_work(res)
+
+#test_compare_work()
+
+def print_result_span(result_s):
+	""" done """
+	print(tabulate.tabulate(result_s,
+							headers=['n', 'S_1', 'S_2'],
+							floatfmt=".3f",
+							tablefmt="github"))
+
+def compare_span(span_fn1, span_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
+	"""
+	Compare the values of different recurrences for 
+	given input sizes.
+
+	Returns:
+	A list of tuples of the form
+	(n, span_fn1(n), span_fn2(n), ...)
+	
+	"""
+	result_s = []
+	for n in sizes:
+		# compute W(n) using current a, b, f
+		result_s.append((
+			n,
+			span_fn1(n),
+			span_fn2(n)
+			))
+	return result_s
 
 def test_compare_span():
-	# TODO
+	# curry span_calc to create multiple span
+	# functions taht can be passed to compare_span
+    
+	# create span_fn1
+	# create span_fn2
+
+    def span_fn1(n):
+        return span_calc(n, 2, 2, lambda n: 1)
+    
+    def span_fn2(n):
+        return span_calc(n, 2, 2,lambda n: n)
+
+    res = compare_span(span_fn1, span_fn2)
+    print_result_span(res)
+
+#test_compare_span()
